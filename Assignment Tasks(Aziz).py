bd9074e9953d8c9c8a6041c6cf2b1c848b29fc01
ShaikAzizpasha <133938701+ShaikAzizpasha@users.noreply.github.com>
2024-07-23 11:24:16 +0530

    This is Assignment Tasks

---
+++ Assignment Tasks(Aziz).py
@@ -0,0 +1,17 @@
+#task1
+print('Welcom to My world')
+print('Hello World')
+
+#task2
+#data types
+#number:
+#integers
+a=123
+#float
+b=1.2
+print(type(a))
+print(type(b))
+#complex
+print(complex(a,b))
+c='1234'
+print(type(c))
